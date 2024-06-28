ffcff90e6e15bdbd58bcc40a6e3f8f8a710cc6ca
Tymoteusz Kaljuta <licwin1410@gmail.com>
2024-06-28 12:04:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const images = {
-    about: [],
+    about: [
+        { text: `<h1>Welcome!</h1>
+                <p>This is blog/archive of my photos and stargazing observations.</p>
+                <p>At the moment I am starting my way in astronomy, I can say I am realizing my childhood dream.</p>
+                <p>The equipment I'm currently using: Sky-Watcher BK 1309 EQ2 130/900, 10mm (90x) and 25mm(36x) Kellner Super, Barlow lens x2. For photos, I use Xiaomi Mi 10T Pro.<p>
+                <p>My github source <a href="https://github.com/escfrpls/astro/">here</a>.</p>` }
+    ],
     sun: [
         { src: 'img/sun1.jpg', caption: 'Sun 1' },
         { src: 'img/sun2.jpg', caption: 'Sun 2' }
     ],
     moon: [
-        { src: 'moon_first_photo_28.06.2024.png', caption: 'First photo Moon, 28.06.2024' },
+        { src: 'img/moon1.jpg', caption: 'Moon 1' },
         { src: 'img/moon2.jpg', caption: 'Moon 2' }
     ],
     solar: [
-        { src: 'Saturn_first_photo_27.06.2024.png', caption: 'First photo Saturn, 27.06.2024' },
+        { src: 'img/solar1.jpg', caption: 'Solar System 1' },
         { src: 'img/solar2.jpg', caption: 'Solar System 2' }
     ],
     galaxy: [
@@ -29,13 +35,21 @@ const images = {
 function showGallery(category) {
     const gallery = document.getElementById('gallery');
     gallery.innerHTML = '';
-    images[category].forEach(image => {
-        const img = document.createElement('img');
-        img.src = image.src;
-        img.alt = image.caption;
-        img.onclick = () => showModal(image.src, image.caption);
-        gallery.appendChild(img);
-    });
+
+    if (category === 'about') {
+        const aboutText = images[category][0].text;
+        const aboutDiv = document.createElement('div');
+        aboutDiv.innerHTML = aboutText;
+        gallery.appendChild(aboutDiv);
+    } else {
+        images[category].forEach(image => {
+            const img = document.createElement('img');
+            img.src = image.src;
+            img.alt = image.caption;
+            img.onclick = () => showModal(image.src, image.caption);
+            gallery.appendChild(img);
+        });
+    }
 }
 
 function showModal(src, caption) {
